1de8ff395fdf054c6f0249ea6578fecec1c47200
Sudria <sudria.gonzalo@gmail.com>
2024-04-12 18:10:20 -0300

    Agregar archivos de proyecto.

---
+++ Controllers/Desafio1Controller.cs
@@ -0,0 +1,55 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace SudriaGonzalo_challenge.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+
+    public class Desafio1Controller : Controller
+    {
+
+        private readonly IHttpClientFactory _httpClientFactory;
+
+        public Desafio1Controller(IHttpClientFactory httpClientFactory)
+        {
+            _httpClientFactory = httpClientFactory;
+        }
+
+
+        [HttpGet("/[controller]/paises/{pais}")]
+        public async Task<IActionResult> ApiCall(String pais)
+        {
+            var httpClient = _httpClientFactory.CreateClient();
+            pais = pais.ToUpper();
+            string apiUrl = "https://api.mercadolibre.com/classified_locations/countries";
+
+            try
+            {
+                HttpResponseMessage response = await httpClient.GetAsync(apiUrl + $"/{pais}");
+
+                if (response.IsSuccessStatusCode)
+                {
+                    if (pais == "AR")
+                    {
+                    string content = await response.Content.ReadAsStringAsync();
+                    return Ok(content);
+                    }
+                    else
+                    {
+                    return Unauthorized("error 401 unauthorized de http");
+                    }
+                }
+                else
+                {
+                    return BadRequest("La solicitud a la API falló.");
+                }
+            }
+            catch (Exception ex)
+            {
+                return StatusCode(500, $"Error: {ex.Message}");
+            }
+        }
+
+    }
+}
